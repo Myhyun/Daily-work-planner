6bafa738a3992ee4d084c309025d0a55e475c7a9
Myhyun <mattyihyun@gmail.com>
2020-06-19 21:56:33 -0700

    dynamically working colors for past/present/future

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,33 @@
 $(function () {
+  updateTime();
+  setInterval(function () {
     updateTime();
-    setInterval(function () {
-      updateTime();
-    }, 1000);
-    function updateTime() {
-      var timeEl = $("#time");
-      var dateEl = $("#date");
-      var now = moment();
-      var formattedTime = now.format("h:mm:ss");
-      var formattedDate = now.format("MM/DD/YYYY");
-      timeEl.text(formattedTime);
-      dateEl.text(formattedDate);
-       $("#currentDay").text(formattedDate);
-    }
-  });
+  }, 1000);
+  function updateTime() {
+    var timeEl = $("#time");
+    var dateEl = $("#date");
+    var now = moment();
+    var formattedTime = now.format("h:mm:ss");
+    var formattedDate = now.format("MM/DD/YYYY");
+    timeEl.text(formattedTime);
+    dateEl.text(formattedDate);
+    $("#currentDay").text(formattedDate);
+  }
+});
 
+
+var currentHour = moment().hour();
+console.log(currentHour);
+
+$(".time-block").each(function(){
+  var timeSlot = parseInt($(this).attr("id"));
+  if (timeSlot > currentHour) {
+    $(this).attr("class", "row time-block future");
+
+  } else if (timeSlot < currentHour) {
+    $(this).attr("class", "row time-block past");
+
+  } else if (timeSlot === currentHour)
+    $(this).attr("class", "row time-block present");
+});
 
