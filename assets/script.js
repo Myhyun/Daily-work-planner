9417656648e3e0f604ebd21fbb04777fd85fdabb
Myhyun <mattyihyun@gmail.com>
2020-06-20 13:02:30 -0700

    added working save button, however; values appearing as null

--- assets/script.js
+++ assets/script.js
@@ -13,31 +13,27 @@ $(function () {
     dateEl.text(formattedDate);
     $("#currentDay").text(formattedDate);
   }
+
+  var currentHour = moment().hour();
+  console.log(currentHour);
+
+  $(".time-block").each(function () {
+    var timeSlot = parseInt($(this).attr("id"));
+    if (timeSlot > currentHour) {
+      $(this).attr("class", "row time-block future");
+
+    } else if (timeSlot < currentHour) {
+      $(this).attr("class", "row time-block past");
+
+    } else if (timeSlot === currentHour)
+      $(this).attr("class", "row time-block present");
+  });
+
+  $(".saveBtn").on('click', function () {
+    var userInput = $(".col-md-10 description").value;
+    var userInputs = [userInput]
+    var plans = localStorage.setItem('plans', JSON.stringify(userInputs));
+    console.log(plans)
+    
+  });
 });
-
-
-var currentHour = moment().hour();
-console.log(currentHour);
-
-$(".time-block").each(function(){
-  var timeSlot = parseInt($(this).attr("id"));
-  if (timeSlot > currentHour) {
-    $(this).attr("class", "row time-block future");
-
-  } else if (timeSlot < currentHour) {
-    $(this).attr("class", "row time-block past");
-
-  } else if (timeSlot === currentHour)
-    $(this).attr("class", "row time-block present");
-});
-
-
-var plans = JSON.parse(localStorage.getItem("plans"));
-var planArr = [];
-localStorage.setItem("plans", JSON.stringify(planArr));
-
-
-
-
-
-
